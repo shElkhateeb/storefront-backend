370ecd45f3b7fb35f8d6814ced85b2b43e05da3d
Shorouk Elkhateeb <shrouca@gmail.com>
2022-06-20 15:03:31 +0200

    test: Add current order test

--- src/tests/models/order_spec.ts
+++ src/tests/models/order_spec.ts
@@ -1,5 +1,5 @@
 import { Order, OrderStore } from '../../models/order';
-import { UserStore } from '../../models/user';
+import { User, UserStore } from '../../models/user';
 import { Product, ProductStore } from '../../models/product';
 import Client from '../../database';
 
@@ -9,60 +9,49 @@ const pStore = new ProductStore();
 
 describe('Order Model', () => {
 	describe('Exsistance of Order Methods', () => {
-		it('should have an index method', () => {
+		it('should have a current order method', () => {
 			expect(store.currentOrder).toBeDefined();
-		});
-		it('should have a create method', () => {
-			expect(store.create).toBeDefined();
-		});
-		it('should have a delete method', () => {
-			expect(store.addProduct).toBeDefined();
 		});
 	});
 
 	describe('Functionality of Order Methods', async () => {
-		const user = await uStore.create({
-			email: 'om@mail.com',
-			first_name: 'Test',
-			last_name: 'User',
-			password: 'pass1234',
-		});
+		let user: User;
+		let product: Product;
+		let order: Order;
 
-		const product = await pStore.create({
-			name: 'P1',
-			price: 10,
+		beforeAll(async()=>{
+			user = await uStore.create({
+				email: 'om@mail.com',
+				first_name: 'Test',
+				last_name: 'User',
+				password: 'pass1234',
+			});
+			product = await pStore.create({
+				name: 'P1',
+				price: 10,
+			});
+			order = await store.create(user.id as number);
 		});
 
 		afterAll(async ()=> {
-			/* delete products and users*/
+			/* delete orders, products, and users*/
 					//open connection
 					const conn = await Client.connect();
-					//delete products
-					let sql = 'DELETE FROM products; DELETE FROM users';
+					//delete orders, products, users
+					let sql = 'DELETE FROM orders; DELETE FROM products; DELETE FROM users';
 					conn.query(sql);
 					// reset id
-					sql = 'ALTER SEQUENCE products_id_seq RESTART WITH 1; ALTER SEQUENCE users_id_seq RESTART WITH 1';
+					sql = 'ALTER SEQUENCE orders_id_seq RESTART WITH 1; ALTER SEQUENCE products_id_seq RESTART WITH 1; ALTER SEQUENCE users_id_seq RESTART WITH 1';
 					conn.query(sql);
 					//release connection
 					conn.release();
 		});
-
-		it('create method should place a new order', async () => {
-			const result = await store.create(user.id as number);
-			expect(result.id).toEqual(1);
-			expect(result.status_of_order).toEqual('active');
-			expect(result.user_id).toEqual(user.id as number);
-			expect(result.products).toEqual([]);
+		it ('Current order of user', async()=>{
+			const result = await store.currentOrder(user.id as number);
+			expect(result.id).toBe(order.id);
+			expect(result.status_of_order).toBe('active');
 		});
 
-		it('addProduct should add product to an exsisting order', async () => {
-			const result = await store.addProduct(
-				5,
-				user.id as number,
-				product.id as number
-			);
-			expect(result.id).toEqual(1);
-			expect(result.user_id).toEqual(1);
-		});
 	});
+	
 });
